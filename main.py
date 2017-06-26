66994f14b4efebb6bb03d6129dc6363e9e5020a7
krisztian <alt.krisztian@gmail.com>
2017-06-26 20:07:34 +0200

    Delete Python based show table code lines

--- main.py
+++ main.py
@@ -2,17 +2,10 @@ from flask import Flask, render_template, request, redirect, url_for
 import requests
 
 app = Flask(__name__)
-# api_url_planets = 'https://swapi.co/api/planets/'
 
 
 @app.route('/')
 def main_page():
-    # planet_request = requests.get(api_url_planets)
-    # previous_page = planet_request.json()["previous"]
-    # next_page = planet_request.json()["next"]
-    # planet_datas = planet_request.json()["results"]
-    # print(previous_page, next_page)
-    # return render_template('index.html', planet_datas=planet_datas)
     return render_template('index.html')
 
 
