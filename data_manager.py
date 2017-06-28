f272fe4084701244de85775d792e808cce0f173c
krisztian <alt.krisztian@gmail.com>
2017-06-28 11:38:04 +0200

    Create database in Python and psql

---
+++ data_manager.py
@@ -0,0 +1,40 @@
+import psycopg2
+
+
+def database_connection():
+    try:
+        connect_str = "dbname='krisztian' user='krisztian' host='localhost'"
+        conn = psycopg2.connect(connect_str)
+        conn.autocommit = True
+        cursor = conn.cursor()
+    except Exception as e:
+        print("There is no connection. Invalid dbname, user or password? Please, check it.")
+        print(e)
+    return cursor, conn
+
+
+def query_result(*query):
+    try:
+        cursor, conn = database_connection()
+        cursor.execute(*query)
+        rows = cursor.fetchall()
+        # try without list format!
+        # rows = [list(row) for row in rows]
+    except psycopg2.OperationalError as e:
+        print('OperationalError')
+        print(e)
+        rows = ""
+    except psycopg2.ProgrammingError as e:
+        print("Nothing to print")
+        print(e)
+        rows = ""
+    except psycopg2.IntegrityError as e:
+        print('IntegrityError')
+        print(e)
+        rows = ""
+        # raise e from query_result()
+    finally:
+        if conn:
+            conn.close()
+    return rows
+
---
+++ the_world_of_Star_Wars_sample_data.sql
@@ -0,0 +1,38 @@
+-- PostgreSQL database dump
+
+ALTER TABLE IF EXISTS ONLY public.sw_users DROP CONSTRAINT IF EXISTS pk_sw_user_id CASCADE;
+ALTER TABLE IF EXISTS ONLY public.sw_planet_votes DROP CONSTRAINT IF EXISTS pk_sw_planet_votes_id CASCADE;
+ALTER TABLE IF EXISTS ONLY public.sw_planet_votes DROP CONSTRAINT IF EXISTS fk_sw_user_id CASCADE;
+
+DROP TABLE IF EXISTS public.sw_users;
+DROP SEQUENCE IF EXISTS public.sw_users_id_seq;
+CREATE TABLE sw_users (
+    id serial NOT NULL, -- primary key
+    username varchar,
+    password varchar
+);
+
+DROP TABLE IF EXISTS public.sw_planet_votes;
+DROP SEQUENCE IF EXISTS public.sw_planet_votes_id_seq;
+CREATE TABLE sw_planet_votes (
+    id serial NOT NULL, -- primary key
+    planet_id int, -- id from SWAPI data
+    planet_name varchar,
+    user_id int, -- foreign key
+    submission_time timestamp without time zone
+);
+
+
+ALTER TABLE ONLY sw_users
+    ADD CONSTRAINT pk_sw_user_id PRIMARY KEY (id);
+
+ALTER TABLE ONLY sw_planet_votes
+    ADD CONSTRAINT pk_sw_planet_votes_id PRIMARY KEY (id);
+
+ALTER TABLE ONLY sw_planet_votes
+    ADD CONSTRAINT fk_sw_user_id FOREIGN KEY (user_id) REFERENCES sw_users(id)
+    ON DELETE CASCADE;
+
+INSERT INTO sw_users VALUES (1, 'Freddie Mercury', 'bohemian');
+INSERT INTO sw_users VALUES (2, 'Lord Vader', 'darkside');
+SELECT pg_catalog.setval('sw_users_id_seq', 2, true);
