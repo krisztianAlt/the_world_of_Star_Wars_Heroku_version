0455d92cc52a9e512151842f814e77f466bc6d5e
krisztianAlt <alt.krisztian@gmail.com>
2020-10-28 21:28:16 +0100

    Refresh swapi's URL

--- static/main.js
+++ static/main.js
@@ -1,7 +1,7 @@
 var app = app || {};
 
 app.init = function() {
-    app.tableHandler.loadTable('https://swapi.co/api/planets/');
+    app.tableHandler.loadTable('https://swapi.dev/api/planets/');
     app.tableHandler.showTurningPageButtons();
     app.tableHandler.loginListener();
 }
